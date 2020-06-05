f8e89054ab82b7a00d2b23b7bcf55c92ba088d8c
Svein Are Danielsen <sveindani@gmail.com>
2020-06-05 14:02:49 +0200

    added admin & can-read guards

---
+++ src/app/core/guards/admin.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+import { take, tap, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminGuard implements CanActivate {
+  constructor(private auth: AuthService) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.auth.user$.pipe(
+      take(1),
+      map((user) => (user && user.roles.admin ? true : false)),
+      tap((isAdmin) => {
+        if (!isAdmin) {
+          console.error('Access denied - Admins only');
+        }
+      })
+    );
+  }
+}
